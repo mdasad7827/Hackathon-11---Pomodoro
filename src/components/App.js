acd6ba159c5ae616ff1d99cdca2789c30d6f0028
mdasad7827 <mohaammadasad1996@gmail.com>
2020-12-29 04:05:49 +0530

    j

--- src/components/App.js
+++ src/components/App.js
@@ -75,7 +75,9 @@ const App = () => {
         <h1 className="timer">{`${appendZero(minutes)}:${appendZero(
           seconds
         )}`}</h1>
-        <h3 className="duration">{`${durationTime}-Time`}</h3>
+        <h3 className="duration">
+          {durationTime === "Work" ? "work" : "break"}-Time
+        </h3>
       </div>
       <div className="controls">
         <button
