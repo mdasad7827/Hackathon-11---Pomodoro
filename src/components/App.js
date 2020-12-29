b4de26436fab23029a70bca99fad7bf1fd3ac751
mdasad7827 <mohaammadasad1996@gmail.com>
2020-12-29 12:37:30 +0530

    set Time

--- src/components/App.js
+++ src/components/App.js
@@ -1,315 +1,315 @@
-// import React, { useEffect, useState } from "react";
-// import "../styles/App.css";
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 
-// const App = () => {
-//   const [minutes, setMinutes] = useState(25);
-//   const [breakDuration, setBreakDuration] = useState(5);
+const App = () => {
+  const [minutes, setMinutes] = useState(25);
+  const [breakDuration, setBreakDuration] = useState(5);
 
-//   const [seconds, setSeconds] = useState(0);
-//   const [newMinutes, setNewMinutes] = useState(minutes);
-//   const [newBreakDuration, setNewBreakDuration] = useState(breakDuration);
-//   const [durationTime, setDurationTime] = useState("Work-Time");
-//   const [start, setStart] = useState(true);
-//   const [stop, setStop] = useState(false);
-//   const [reset, setReset] = useState(true);
+  const [seconds, setSeconds] = useState(0);
+  const [newMinutes, setNewMinutes] = useState(minutes);
+  const [newBreakDuration, setNewBreakDuration] = useState(breakDuration);
+  const [durationTime, setDurationTime] = useState("Work");
+  const [start, setStart] = useState(true);
+  const [stop, setStop] = useState(false);
+  const [reset, setReset] = useState(true);
 
-//   const appendZero = (num) => {
-//     if (num < 10) return `0${num}`;
-//     return num;
-//   };
-
-//   useEffect(() => {
-//     if (!start) {
-//       const intervalId = setInterval(() => {
-//         if (Number(minutes) === 0 && Number(seconds) === 0) {
-//           if (durationTime === "Work-Time") {
-//             setMinutes(breakDuration);
-//             setDurationTime("Break-Time");
-//             alert("work duration is over");
-//           } else {
-//             setMinutes(newMinutes);
-//             setDurationTime("Work-Time");
-//             alert("break duration is over");
-//           }
-//         } else if (Number(seconds) === 0 && Number(minutes) > 0) {
-//           setMinutes(minutes - 1);
-//           setSeconds(59);
-//         } else if (Number(seconds) > 0) {
-//           setSeconds(seconds - 1);
-//         }
-//       }, 1 * 1000);
-//       return () => clearInterval(intervalId);
-//     }
-//   });
-
-//   function resetHandler() {
-//     setNewMinutes(25);
-//     setBreakDuration(5);
-//     setMinutes(25);
-//     setSeconds(0);
-//     setNewBreakDuration(5);
-//     setDurationTime("Work-Time");
-//   }
-
-//   function setHandler() {
-//     if (newMinutes != 0 || newBreakDuration != 0) {
-//       setMinutes(newMinutes);
-//       setSeconds(0);
-//       setBreakDuration(newBreakDuration);
-//     } else {
-//       resetHandler();
-//     }
-//   }
-
-//   function stopHandler() {
-//     setStart(true);
-//   }
-
-//   return (
-//     <div id="main" style={{ textAlign: "center" }}>
-//       <div className="clock">
-//         <h1 className="timer">{`${appendZero(minutes)}:${appendZero(
-//           seconds
-//         )}`}</h1>
-//         <h3 className="duration">{durationTime}</h3>
-//       </div>
-//       <div className="controls">
-//         <button
-//           data-testid="start-btn"
-//           onClick={() => {
-//             setStart(false);
-//             setStop(true);
-//             setReset(false);
-//           }}
-//           disabled={!start}
-//         >
-//           Start
-//         </button>
-//         <button
-//           data-testid="stop-btn"
-//           onClick={() => {
-//             setStart(true);
-//             setStop(false);
-//             stopHandler();
-//           }}
-//           disabled={start}
-//         >
-//           Stop
-//         </button>
-//         <button
-//           data-testid="reset-btn"
-//           onClick={() => {
-//             setStart(true);
-//             setReset(true);
-//             resetHandler();
-//           }}
-//           disabled={reset || (stop && start)}
-//         >
-//           Reset
-//         </button>
-//       </div>
-//       <br />
-//       <div className="parameters">
-//         <form>
-//           <input
-//             data-testid="work-duration"
-//             placeholder="work-duration"
-//             required
-//             type="number"
-//             min="0"
-//             onChange={(e) => setNewMinutes(e.target.value)}
-//             value={newMinutes}
-//             disabled={!start}
-//           />
-//           <input
-//             data-testid="break-duration"
-//             placeholder="break-duration"
-//             required
-//             type="number"
-//             min="0"
-//             onChange={(e) => setNewBreakDuration(e.target.value)}
-//             value={newBreakDuration}
-//             disabled={!start}
-//           />
-//           <button
-//             data-testid="set-btn"
-//             type="button"
-//             onClick={setHandler}
-//             disabled={!start}
-//           >
-//             Set
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-import React, { useState, useEffect } from "react";
-
-function App() {
-  const [workingdurations, Setworkintimes] = useState(25);
-  const [breakDuration, setRestTime] = useState(5);
-  const [flag, setFlag] = useState(false);
-  const [worksecond, setWorkinSecs] = useState(1500);
-  const [breaksecond, setBreakSecs] = useState(300);
-  const [currentStateofEmp, setCurrentStatus] = useState("work");
-  const [resetFlag, ResetFlag] = useState(true);
+  const appendZero = (num) => {
+    if (num < 10) return `0${num}`;
+    return num;
+  };
 
   useEffect(() => {
-    if (flag && currentStateofEmp === "work") {
-      if (worksecond > 0) {
-        const timer = setTimeout(() => {
-          setWorkinSecs(worksecond - 1);
-        }, 1000);
-        return () => clearTimeout(timer);
-      }
-      if (worksecond === 0) {
-        setCurrentStatus("break");
-        setWorkinSecs(workingdurations * 60);
-        alert("work duration is over");
-      }
+    if (!start) {
+      const intervalId = setInterval(() => {
+        if (Number(minutes) === 0 && Number(seconds) === 0) {
+          if (durationTime === "Work") {
+            setMinutes(breakDuration);
+            setDurationTime("Break");
+            alert("work duration is over");
+          } else {
+            setMinutes(newMinutes);
+            alert("break duration is over");
+            setDurationTime("Work");
+          }
+        } else if (Number(seconds) === 0 && Number(minutes) > 0) {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        } else if (Number(seconds) > 0) {
+          setSeconds(seconds - 1);
+        }
+      }, 1 * 1000);
+      return () => clearInterval(intervalId);
     }
-    if (flag && currentStateofEmp === "break") {
-      if (breaksecond > 0) {
-        const timer = setTimeout(() => {
-          setBreakSecs(breaksecond - 1);
-        }, 1000);
-        return () => clearTimeout(timer);
-      }
-      if (breaksecond === 0) {
-        setBreakSecs(breakDuration * 60);
-        alert("break duration is over");
-        setCurrentStatus("work");
-      }
-    }
-  }, [
-    workingdurations,
-    breakDuration,
-    breaksecond,
-    flag,
-    currentStateofEmp,
-    worksecond,
-  ]);
+  });
 
-  function reset() {
-    console.log("INSIDE RESET");
-    ResetFlag(true);
-    setFlag(false);
-    setCurrentStatus("work");
-    Setworkintimes(25);
-    setRestTime(5);
-    setBreakSecs(300);
-    setWorkinSecs(1500);
+  function resetHandler() {
+    setNewMinutes(25);
+    setBreakDuration(5);
+    setMinutes(25);
+    setSeconds(0);
+    setNewBreakDuration(5);
+    setDurationTime("Work");
   }
-  const sectomins = (secs) => {
-    console.log(secs);
-    // console.log(parseInt(secs/60))
-    // console.log(parseInt(secs%60))
-    let minis = parseInt(secs / 60) + "";
-    let secos = parseInt(secs % 60) + "";
-    if (minis.length === 1) {
-      minis = "0" + minis;
+
+  function setHandler() {
+    if (newMinutes != 0 || newBreakDuration != 0) {
+      setMinutes(newMinutes);
+      setSeconds(0);
+      setBreakDuration(newBreakDuration);
+    } else {
+      resetHandler();
     }
-    if (secos.length === 1) {
-      secos = "0" + secos;
-    }
-    return minis + ":" + secos;
-  };
-  const validateData = (d) => {
-    try {
-      if (!isNaN(d) && parseInt(d) >= 0) {
-        return parseInt(d);
-      } else return "";
-    } catch (err) {}
-  };
-  const setEnteredTime = (e) => {
-    console.log(e);
-    if (breakDuration + workingdurations <= 0) {
-      reset();
-      return;
-    }
-    ResetFlag(false);
-    setCurrentStatus("work");
-    setWorkinSecs(workingdurations * 60);
-    setBreakSecs(breakDuration * 60);
-  };
+  }
+
+  function stopHandler() {
+    setStart(true);
+  }
+
   return (
-    <div className="App" style={{ textAlign: "center" }}>
+    <div id="main" style={{ textAlign: "center" }}>
       <div className="clock">
-        <h1 className="timer">
-          {currentStateofEmp === "work"
-            ? sectomins(worksecond)
-            : sectomins(breaksecond)}
-        </h1>
-        <h3>{currentStateofEmp === "work" ? "Work" : "Break"}-Time</h3>
+        <h1 className="timer">{`${appendZero(minutes)}:${appendZero(
+          seconds
+        )}`}</h1>
+        <h3 className="duration">{durationTime}-Time</h3>
       </div>
-      <div>
+      <div className="controls">
+        <button
+          data-testid="start-btn"
+          onClick={() => {
+            setStart(false);
+            setStop(true);
+            setReset(false);
+          }}
+          disabled={!start}
+        >
+          Start
+        </button>
         <button
           data-testid="stop-btn"
-          key="stop"
           onClick={() => {
-            setFlag(false);
-            ResetFlag(false);
+            setStart(true);
+            setStop(false);
+            stopHandler();
           }}
-          disabled={!flag}
+          disabled={start}
         >
           Stop
         </button>
         <button
-          data-testid="start-btn"
-          key="start"
-          onClick={() => {
-            setFlag(true);
-            ResetFlag(false);
-          }}
-          disabled={flag}
-        >
-          start
-        </button>
-        <button
           data-testid="reset-btn"
-          key="reset"
           onClick={() => {
-            reset();
+            setStart(true);
+            setReset(true);
+            resetHandler();
           }}
-          disabled={resetFlag}
+          disabled={reset || (stop && start)}
         >
           Reset
         </button>
       </div>
-      <br></br>
-      <div>
-        <input
-          data-testid="work-duration"
-          placeholder="work duration"
-          required
-          type="Number"
-          value={workingdurations}
-          disabled={flag}
-          onChange={(e) => Setworkintimes(validateData(e.target.value))}
-        ></input>
-        <input
-          data-testid="break-duration"
-          placeholder="break duration"
-          required
-          type="Number"
-          value={breakDuration}
-          disabled={flag}
-          onChange={(e) => setRestTime(validateData(e.target.value))}
-        ></input>
-        <button
-          onClick={setEnteredTime}
-          data-testid="set-btn"
-          type="submit"
-          disabled={flag}
-        >
-          set
-        </button>
+      <br />
+      <div className="parameters">
+        <form>
+          <input
+            data-testid="work-duration"
+            placeholder="work-duration"
+            required
+            type="number"
+            min="0"
+            onChange={(e) => setNewMinutes(e.target.value)}
+            value={newMinutes}
+            disabled={!start}
+          />
+          <input
+            data-testid="break-duration"
+            placeholder="break-duration"
+            required
+            type="number"
+            min="0"
+            onChange={(e) => setNewBreakDuration(e.target.value)}
+            value={newBreakDuration}
+            disabled={!start}
+          />
+          <button
+            data-testid="set-btn"
+            type="button"
+            onClick={setHandler}
+            disabled={!start}
+          >
+            Set
+          </button>
+        </form>
       </div>
     </div>
   );
-}
+};
+
 export default App;
+
+// import React, { useState, useEffect } from "react";
+
+// function App() {
+//   const [workingdurations, Setworkintimes] = useState(25);
+//   const [breakDuration, setRestTime] = useState(5);
+//   const [flag, setFlag] = useState(false);
+//   const [worksecond, setWorkinSecs] = useState(1500);
+//   const [breaksecond, setBreakSecs] = useState(300);
+//   const [currentStateofEmp, setCurrentStatus] = useState("work");
+//   const [resetFlag, ResetFlag] = useState(true);
+
+//   useEffect(() => {
+//     if (flag && currentStateofEmp === "work") {
+//       if (worksecond > 0) {
+//         const timer = setTimeout(() => {
+//           setWorkinSecs(worksecond - 1);
+//         }, 1000);
+//         return () => clearTimeout(timer);
+//       }
+//       if (worksecond === 0) {
+//         setCurrentStatus("break");
+//         setWorkinSecs(workingdurations * 60);
+//         alert("work duration is over");
+//       }
+//     }
+//     if (flag && currentStateofEmp === "break") {
+//       if (breaksecond > 0) {
+//         const timer = setTimeout(() => {
+//           setBreakSecs(breaksecond - 1);
+//         }, 1000);
+//         return () => clearTimeout(timer);
+//       }
+//       if (breaksecond === 0) {
+//         setBreakSecs(breakDuration * 60);
+//         alert("break duration is over");
+//         setCurrentStatus("work");
+//       }
+//     }
+//   }, [
+//     workingdurations,
+//     breakDuration,
+//     breaksecond,
+//     flag,
+//     currentStateofEmp,
+//     worksecond,
+//   ]);
+
+//   function reset() {
+//     console.log("INSIDE RESET");
+//     ResetFlag(true);
+//     setFlag(false);
+//     setCurrentStatus("work");
+//     Setworkintimes(25);
+//     setRestTime(5);
+//     setBreakSecs(300);
+//     setWorkinSecs(1500);
+//   }
+//   const sectomins = (secs) => {
+//     console.log(secs);
+//     // console.log(parseInt(secs/60))
+//     // console.log(parseInt(secs%60))
+//     let minis = parseInt(secs / 60) + "";
+//     let secos = parseInt(secs % 60) + "";
+//     if (minis.length === 1) {
+//       minis = "0" + minis;
+//     }
+//     if (secos.length === 1) {
+//       secos = "0" + secos;
+//     }
+//     return minis + ":" + secos;
+//   };
+//   const validateData = (d) => {
+//     try {
+//       if (!isNaN(d) && parseInt(d) >= 0) {
+//         return parseInt(d);
+//       } else return "";
+//     } catch (err) {}
+//   };
+//   const setEnteredTime = (e) => {
+//     console.log(e);
+//     if (breakDuration + workingdurations <= 0) {
+//       reset();
+//       return;
+//     }
+//     ResetFlag(false);
+//     setCurrentStatus("work");
+//     setWorkinSecs(workingdurations * 60);
+//     setBreakSecs(breakDuration * 60);
+//   };
+//   return (
+//     <div className="App" style={{ textAlign: "center" }}>
+//       <div className="clock">
+//         <h1 className="timer">
+//           {currentStateofEmp === "work"
+//             ? sectomins(worksecond)
+//             : sectomins(breaksecond)}
+//         </h1>
+//         <h3>{currentStateofEmp === "work" ? "Work" : "Break"}-Time</h3>
+//       </div>
+//       <div>
+//         <button
+//           data-testid="stop-btn"
+//           key="stop"
+//           onClick={() => {
+//             setFlag(false);
+//             ResetFlag(false);
+//           }}
+//           disabled={!flag}
+//         >
+//           Stop
+//         </button>
+//         <button
+//           data-testid="start-btn"
+//           key="start"
+//           onClick={() => {
+//             setFlag(true);
+//             ResetFlag(false);
+//           }}
+//           disabled={flag}
+//         >
+//           start
+//         </button>
+//         <button
+//           data-testid="reset-btn"
+//           key="reset"
+//           onClick={() => {
+//             reset();
+//           }}
+//           disabled={resetFlag}
+//         >
+//           Reset
+//         </button>
+//       </div>
+//       <br></br>
+//       <div>
+//         <input
+//           data-testid="work-duration"
+//           placeholder="work duration"
+//           required
+//           type="Number"
+//           value={workingdurations}
+//           disabled={flag}
+//           onChange={(e) => Setworkintimes(validateData(e.target.value))}
+//         ></input>
+//         <input
+//           data-testid="break-duration"
+//           placeholder="break duration"
+//           required
+//           type="Number"
+//           value={breakDuration}
+//           disabled={flag}
+//           onChange={(e) => setRestTime(validateData(e.target.value))}
+//         ></input>
+//         <button
+//           onClick={setEnteredTime}
+//           data-testid="set-btn"
+//           type="submit"
+//           disabled={flag}
+//         >
+//           set
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+// export default App;
